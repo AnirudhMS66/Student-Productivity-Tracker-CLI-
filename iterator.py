66a0c9db897373751f2bb2cce28ce94f0481e342
AnirudhMS66 <ak22sset@gmail.com>
2026-02-11 11:47:15 +0530

    new commit

--- iterator.py
+++ iterator.py
@@ -10,3 +10,21 @@ print(next(ite))
 print(next(ite))
 print(next(ite))
 
+class count:
+    def __init__(self,max):
+        self.num=0
+        self.max=max
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.num<self.max:
+            val=self.num
+            self.num+=1
+            return val
+        else:
+            raise StopIteration
+
+c=count(3);
+print(next(c))
+print(next(c))
+print(next(c))
